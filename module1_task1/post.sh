1d619679a8f9c4a671a55276bda34d7ddae01f40
s4iTen <ychouk102@gmail.com>
2023-05-09 13:05:19 +0200

    this is the fixing of the bugs in te post script

--- module1_task1/post.sh
+++ module1_task1/post.sh
@@ -3,9 +3,6 @@
 
 post_title="$POST_TITLE"
 post_name="$POST_NAME"
-if [ -z "$post_title" ] || [ -z "$post_name" ]; then
-  exit 1
-fi
 hugo new "posts/$post_name.md"
 post_file="content/posts/$post_name.md"
 sed -i "1s/^title: .*$/title: \"$post_title\"/" "$post_file"
