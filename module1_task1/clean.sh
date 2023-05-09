b1ff9e8a32221eb98e4f7b180fd49779edcf97b4
s4iTen <ychouk102@gmail.com>
2023-05-09 13:17:34 +0200

    this is the last udpates

--- module1_task1/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd dist
-rm -rf *
--- module1_task1/post.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-post_title="$POST_TITLE"
-post_name="$POST_NAME"
-hugo new "posts/$post_name.md"
-sed -i "1s/^title: .*$/title: \"$post_title\"/" "/content/posts/$post_name"
