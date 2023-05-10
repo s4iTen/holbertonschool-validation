d8d0b23e9df2e14b57c2a8c7ec2b5c49de381505
s4iTen <ychouk102@gmail.com>
2023-05-10 23:42:53 +0200

    this is the command that can install the make and the hugo without fail

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+sudo mv hugo /usr/local/bin/
+sudo rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+
 make build
